562386ad1864d5ef7b1a8993da8d13a590e1da37
Apurv Singh <apurav12345@gmail.com>
2020-08-09 10:18:34 +0530

    first

---
+++ app/src/main/java/work/newproject/asus/apurv/lms/auth/LoginFragment.java
@@ -0,0 +1,23 @@
+package work.newproject.asus.apurv.lms.auth;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import work.newproject.asus.apurv.lms.R;
+
+
+public class LoginFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
