06a2106eab41dd3dc301f379b1622b3db5de7cde
kushwahamadhulika <47361898+kushwahamadhulika@users.noreply.github.com>
2020-08-11 01:15:11 -0700

    AdminDashboard

---
+++ app/src/main/java/work/newproject/asus/apurv/lms/Admin/AdminDashboard.java
@@ -0,0 +1,16 @@
+package work.newproject.asus.apurv.lms.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import work.newproject.asus.apurv.lms.R;
+
+public class AdminDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
